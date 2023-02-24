f7538da0405bf1d1302425d0578bb1d5aea1bc6c
Adeleye Ifaturoti <adeleyeifaturoti@yahoo.com>
2023-02-24 16:26:52 +0000

    problem with postAdded

---
+++ src/features/posts/postsSlice.js
@@ -0,0 +1,40 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = [
+  {
+    id: "1",
+    title: "Learning Redux Toolkit",
+    content: "I've heard good things.",
+  },
+  {
+    id: "2",
+    title: "Slices...",
+    content: "The more I say slice, the more I want pizza.",
+  },
+];
+const postsSlice = createSlice({
+  name: "posts",
+  initialState,
+  reducer: {
+    postAdded: {
+      reducers(state, action) {
+        state.push(action.payload);
+      },
+      prepare(title, content) {
+        return {
+          payload: {
+            id: nanoid(),
+            title,
+            content,
+          },
+        };
+      },
+    },
+  },
+});
+
+export const selectAllPosts = (state) => state.posts;
+
+export const { postAdded } = postsSlice.actions;
+
+export default postsSlice.reducer;
